689bf32905ce14baeee0ef43eab0c30e22a0f8d4
Leo Kim <leo@hotbee.co>
2025-01-15 09:34:38 +0900

    SC-201 Remove sandbox projects

--- ConsoleSandbox/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using ControlBee;
-using log4net.Config;
-
-XmlConfigurator.Configure(new FileInfo("log4net.config"));
-
-Console.WriteLine("Hello, World!");
