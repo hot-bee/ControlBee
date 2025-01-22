09674c02b8f1108ec2baebd5f02325bbfd741ad6
Leo Kim <leo@hotbee.co>
2025-01-22 23:43:54 +0900

    SC-255 Introduce DoubleActingActurator

---
+++ ControlBee.Tests/Models/DoubleActingActuatorTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using ControlBee.Exceptions;
+using ControlBee.Interfaces;
+using ControlBee.Models;
+using ControlBee.Services;
+using ControlBee.Variables;
+using JetBrains.Annotations;
+using Moq;
+using Xunit;
+
+namespace ControlBee.Tests.Models;
+
+[TestSubject(typeof(DoubleActingActuator))]
+public class DoubleActingActuatorTest
+{
+    [Fact]
+    public void OnAndWaitTest()
+    {
+        var systemConfigurations = new SystemConfigurations { FakeMode = true };
+        var deviceManger = Mock.Of<IDeviceManager>();
+        var scenarioFlowTester = new ScenarioFlowTester();
+        using var timeManager = new FrozenTimeManager(new FrozenTimeManagerConfig());
+        var digitalInputFactory = new DigitalInputFactory(
+            systemConfigurations,
+            deviceManger,
+            scenarioFlowTester
+        );
+        var digitalOutputFactory = new DigitalOutputFactory(systemConfigurations, deviceManger);
+        var actorRegistry = new ActorRegistry();
+        var actorFactory = new ActorFactory(
+            EmptyAxisFactory.Instance,
+            digitalInputFactory,
+            digitalOutputFactory,
+            EmptyVariableManager.Instance,
+            timeManager,
+            EmptyActorItemInjectionDataSource.Instance,
+            actorRegistry
+        );
+        var ui = Mock.Of<IUiActor>();
+        Mock.Get(ui).Setup(m => m.Name).Returns("ui");
+        actorRegistry.Add(ui);
+        var actor = actorFactory.Create<TestActor>("myActor");
+        scenarioFlowTester.Setup(
+            [
+                [
+                    // ReSharper disable once AccessToDisposedClosure
+                    new ConditionStep(() => timeManager.CurrentMilliseconds > 1000),
+                    new BehaviorStep(() => ((FakeDigitalInput)actor.CylFwdDet).On = true),
+                ],
+            ]
+        );
+
+        actor.Start();
+        actor.Send(new Message(EmptyActor.Instance, "go"));
+        actor.Send(new Message(EmptyActor.Instance, "_terminate"));
+        actor.Join();
+
+        var match = new Func<Message, bool>(message => message.Name == "_requestDialog");
+        Mock.Get(ui).Verify(m => m.Send(It.Is<Message>(message => match(message))), Times.Never);
+        Assert.True(timeManager.CurrentMilliseconds is > 1000 and < 2000);
+    }
+
+    [Fact]
+    public void OnAndTimeoutTest()
+    {
+        var systemConfigurations = new SystemConfigurations { FakeMode = true };
+        var deviceManger = Mock.Of<IDeviceManager>();
+        var scenarioFlowTester = new ScenarioFlowTester();
+        using var timeManager = new FrozenTimeManager(new FrozenTimeManagerConfig());
+        var digitalInputFactory = new DigitalInputFactory(
+            systemConfigurations,
+            deviceManger,
+            scenarioFlowTester
+        );
+        var digitalOutputFactory = new DigitalOutputFactory(systemConfigurations, deviceManger);
+        var actorRegistry = new ActorRegistry();
+        var actorFactory = new ActorFactory(
+            EmptyAxisFactory.Instance,
+            digitalInputFactory,
+            digitalOutputFactory,
+            EmptyVariableManager.Instance,
+            timeManager,
+            EmptyActorItemInjectionDataSource.Instance,
+            actorRegistry
+        );
+        var ui = Mock.Of<IUiActor>();
+        Mock.Get(ui).Setup(m => m.Name).Returns("ui");
+        actorRegistry.Add(ui);
+        var actor = actorFactory.Create<TestActor>("myActor");
+
+        actor.Start();
+        actor.Send(new Message(EmptyActor.Instance, "go"));
+        actor.Send(new Message(EmptyActor.Instance, "_terminate"));
+        actor.Join();
+
+        var match = new Func<Message, bool>(message => message.Name == "_requestDialog");
+        Mock.Get(ui).Verify(m => m.Send(It.Is<Message>(message => match(message))), Times.Once);
+        Assert.True(timeManager.CurrentMilliseconds >= 5000);
+    }
+
+    public class TestActor : Actor
+    {
+        public DoubleActingActuator Cyl;
+
+        public IDigitalOutput CylBwd;
+        public IDigitalInput CylBwdDet;
+        public IDigitalOutput CylFwd;
+        public IDigitalInput CylFwdDet;
+
+        public TestActor(ActorConfig config)
+            : base(config)
+        {
+            CylBwdDet = DigitalInputFactory.Create();
+            CylFwdDet = DigitalInputFactory.Create();
+
+            CylBwd = DigitalOutputFactory.Create();
+            CylFwd = DigitalOutputFactory.Create();
+
+            Cyl = new DoubleActingActuator(CylBwd, CylFwd, CylBwdDet, CylFwdDet);
+        }
+
+        protected override void ProcessMessage(Message message)
+        {
+            if (message.Name == "go")
+                try
+                {
+                    Cyl.OnAndWait(5000);
+                }
+                catch (TimeoutError)
+                {
+                    // Alert trigger will be checked.
+                }
+
+            base.ProcessMessage(message);
+        }
+    }
+}
---
+++ ControlBee/Models/DoubleActingActuator.cs
@@ -0,0 +1,71 @@
+﻿using ControlBee.Interfaces;
+
+namespace ControlBee.Models;
+
+public class DoubleActingActuator(
+    IDigitalOutput outputOff,
+    IDigitalOutput outputOn,
+    IDigitalInput? inputOff,
+    IDigitalInput? inputOn
+) : ActorItem
+{
+    private bool _on;
+
+    public bool On
+    {
+        get => _on;
+        set
+        {
+            _on = value;
+            outputOff.On = !_on;
+            outputOn.On = _on;
+        }
+    }
+
+    public bool Off
+    {
+        get => !On;
+        set => On = !value;
+    }
+
+    public bool IsOn
+    {
+        get
+        {
+            if (Off)
+                return false;
+            return inputOn == null || inputOn.IsOn;
+        }
+    }
+
+    public bool IsOff
+    {
+        get
+        {
+            if (On)
+                return false;
+            return inputOff == null || inputOff.IsOn;
+        }
+    }
+
+    public void OnAndWait(int millisecondsTimeout)
+    {
+        On = true;
+        inputOn?.WaitOn(millisecondsTimeout);
+    }
+
+    public void OffAndWait(int millisecondsTimeout)
+    {
+        On = false;
+        inputOff?.WaitOn(millisecondsTimeout);
+    }
+
+    public override void ProcessMessage(ActorItemMessage message) { }
+
+    public override void UpdateSubItem() { }
+
+    public override void InjectProperties(IActorItemInjectionDataSource dataSource)
+    {
+        // TODO
+    }
+}
