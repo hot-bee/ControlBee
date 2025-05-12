91840b08f6589c8317bfd233c08432dbe5af27b9
Leo <leo@hotbee.co>
2025-05-12 17:10:17 +0900

    Support DataType in AnalogOutput

--- ControlBee/Models/AnalogOutput.cs
+++ ControlBee/Models/AnalogOutput.cs
@@ -1,4 +1,5 @@
-﻿using ControlBee.Interfaces;
+﻿using ControlBee.Constants;
+using ControlBee.Interfaces;
 using ControlBeeAbstract.Devices;
 using log4net;
 using Dict = System.Collections.Generic.Dictionary<string, object?>;
@@ -8,6 +9,7 @@ namespace ControlBee.Models;
 public class AnalogOutput(IDeviceManager deviceManager) : AnalogIO(deviceManager), IAnalogOutput
 {
     private static readonly ILog Logger = LogManager.GetLogger(nameof(AnalogOutput));
+    public AnalogDataType DataType;
     protected long InternalData;
     protected virtual IAnalogIoDevice? AnalogIoDevice => Device as IAnalogIoDevice;
 
@@ -21,25 +23,32 @@ public class AnalogOutput(IDeviceManager deviceManager) : AnalogIO(deviceManager
             return;
         }
 
-        AnalogIoDevice.SetAnalogOutputSignedDWord(Channel, (int)InternalData);  // TODO: Separate this according to the data size that will be defined in property.
+        switch (DataType)
+        {
+            case AnalogDataType.SignedDWord:
+                AnalogIoDevice.SetAnalogOutputSignedDWord(Channel, (int)InternalData);
+                break;
+            case AnalogDataType.DWord:
+                AnalogIoDevice.SetAnalogOutputDWord(Channel, (uint)InternalData);
+                break;
+            case AnalogDataType.SignedWord:
+                AnalogIoDevice.SetAnalogOutputSignedWord(Channel, (short)InternalData);
+                break;
+            case AnalogDataType.Word:
+                AnalogIoDevice.SetAnalogOutputWord(Channel, (ushort)InternalData);
+                break;
+            case AnalogDataType.SignedByte:
+                AnalogIoDevice.SetAnalogOutputSignedByte(Channel, (sbyte)InternalData);
+                break;
+            case AnalogDataType.Byte:
+                AnalogIoDevice.SetAnalogOutputByte(Channel, (byte)InternalData);
+                break;
+        }
     }
 
     public long Read()
     {
         return InternalData;
-    }
-
-    private void SendDataToUi(Guid requestId)
-    {
-        var payload = new Dict { ["Data"] = InternalData };
-        Actor.Ui?.Send(
-            new ActorItemMessage(requestId, Actor, ItemPath, "_itemDataChanged", payload)
-        );
-    }
-
-    protected virtual void WriteToDevice()
-    {
-        throw new NotImplementedException();
     }
 
     public override bool ProcessMessage(ActorItemMessage message)
@@ -58,5 +67,28 @@ public class AnalogOutput(IDeviceManager deviceManager) : AnalogIO(deviceManager
         }
 
         return base.ProcessMessage(message);
+    }
+
+    public override void InjectProperties(ISystemPropertiesDataSource dataSource)
+    {
+        base.InjectProperties(dataSource);
+        if (
+            dataSource.GetValue(ActorName, ItemPath, nameof(DataType))
+            is string analogDataType
+        )
+            Enum.TryParse(analogDataType, out DataType);
+    }
+
+    private void SendDataToUi(Guid requestId)
+    {
+        var payload = new Dict { ["Data"] = InternalData };
+        Actor.Ui?.Send(
+            new ActorItemMessage(requestId, Actor, ItemPath, "_itemDataChanged", payload)
+        );
+    }
+
+    protected virtual void WriteToDevice()
+    {
+        throw new NotImplementedException();
     }
 }
