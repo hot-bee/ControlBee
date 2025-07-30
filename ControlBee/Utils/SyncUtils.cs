6071f05c858ce5c1bb1de8e54e4cfa7b44f6403d
Leo Kim <leo@hotbee.co>
2025-07-30 14:46:19 +0800

    Introduce SyncRequestsPeek in sync utils

--- ControlBee/Utils/SyncUtils.cs
+++ ControlBee/Utils/SyncUtils.cs
@@ -13,7 +13,21 @@ public class SyncUtils
         string grantName
     )
     {
-        if (requests.Length == 0) return false;
+        var peek = SyncRequestsPeek(actor, grants, requests, grantName);
+        if (!peek.approvable) return false;
+
+        SyncRequestsApprove(actor, grants, requests, grantName, peek.requestIds!);
+        return true;
+    }
+
+    public static (bool approvable, List<Guid>? requestIds) SyncRequestsPeek(
+        Actor actor,
+        Dict grants,
+        RequestSource[] requests,
+        string grantName
+    )
+    {
+        if (requests.Length == 0) return (false, null);
         var guids = new List<Guid>();
         foreach (var request in requests)
         {
@@ -29,20 +43,31 @@ public class SyncUtils
                     actor.GetPeerStatusByActor(request.Actor, lastRequestName)?.Equals(lastGuid)
                     is true
                 )
-                    return false;
+                    return (false, null);
             }
         }
 
         if (guids.Any(x => x == Guid.Empty))
-            return false;
+            return (false, null);
 
         var lastSet = grants.GetValueOrDefault(grantName) as HashSet<Guid> ?? [];
         var newSet = new HashSet<Guid>(guids);
         newSet.IntersectWith(lastSet);
         if (newSet.Count > 0)
-            return false;
+            return (false, null);
 
-        grants[grantName] = new HashSet<Guid>(guids);
+        return (true, guids);
+    }
+
+    public static void SyncRequestsApprove(
+        Actor actor,
+        Dict grants,
+        RequestSource[] requests,
+        string grantName,
+        List<Guid> requestIds
+    )
+    {
+        grants[grantName] = new HashSet<Guid>(requestIds!);
         foreach (var request in requests)
         {
             var guid = GuidUtils.FromObject(
@@ -50,7 +75,5 @@ public class SyncUtils
             );
             grants[request.Actor.Name] = (request.RequestName, guid);
         }
-
-        return true;
     }
 }
