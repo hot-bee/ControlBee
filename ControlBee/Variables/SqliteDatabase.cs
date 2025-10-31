939b4c38e4f0f11235f4642a16d3ad38bef3e30c
kangyunkim <rkddbs0511@naver.com>
2025-10-31 15:39:47 +0900

    Add delete and logout functionality

--- ControlBee/Variables/SqliteDatabase.cs
+++ ControlBee/Variables/SqliteDatabase.cs
@@ -252,7 +252,8 @@ public class SqliteDatabase : IDatabase, IDisposable
                       name        TEXT    NOT NULL,
                       level       INTEGER NOT NULL DEFAULT 0,
                       created_at  TEXT    NOT NULL DEFAULT (datetime('now','localtime')),
-                      updated_at  TEXT    NOT NULL DEFAULT (datetime('now','localtime'))
+                      updated_at  TEXT    NOT NULL DEFAULT (datetime('now','localtime')),
+                      is_deleted  INTEGER NOT NULL DEFAULT 0
                   );
                   """;
         using var command = new SqliteCommand(sql, _connection);
