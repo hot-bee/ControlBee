6dd910e873cb2c510389719e18eaf94934958f60
KangYun <116543523+kangyunkim@users.noreply.github.com>
2025-11-28 18:03:44 +0900

    Add RenameLocalName in VariableManager (#24)
    * Add RenameLocalName in VariableManager
    
    * Use == instead of string.Equals

--- ControlBee/Services/VariableManager.cs
+++ ControlBee/Services/VariableManager.cs
@@ -376,4 +376,12 @@ public class VariableManager(
             throw;
         }
     }
+
+    public void RenameLocalName(string sourceLocalName, string targetLocalName)
+    {
+        bool isCurrent = LocalName == sourceLocalName;
+        database.RenameLocalName(sourceLocalName, targetLocalName);
+        if (isCurrent)
+            Load(targetLocalName);
+    }
 }
